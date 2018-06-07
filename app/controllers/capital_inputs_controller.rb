701c7458e88ac226406351ece08de0152c1b7b15
michael <dworken@powerauctions.com>
2018-06-07 12:16:11 -0400

    add option to manually refresh capital input

--- app/controllers/capital_inputs_controller.rb
+++ app/controllers/capital_inputs_controller.rb
@@ -1,5 +1,12 @@
 class CapitalInputsController < ApplicationController
 
+  def manually_refresh
+  	time_before = Time.now
+    CapitalInput.refresh
+    time_after = Time.now
+    @time_diff = (time_after - time_before).seconds
+    render 'refresh'
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :stations, only: [:index]
   get '/stations/invalid', to: 'stations#invalid'
-  get '/capital_input/update', to: 'capital_input#update'
+  get '/capital_inputs/refresh', to: 'capital_inputs#manually_refresh'
   get '/tester/:station_id', to: 'stations#get_info_by_id'
-
+  
   root 'profile#index'
 end
