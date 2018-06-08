f47d3a8b2bf01e1ebee6c63a805ac100020f1ca3
michael <dworken@powerauctions.com>
2018-06-08 11:59:37 -0400

    set up minitest spec and write first poro test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# not sure which ones I need
+require 'rails/test_help'
+require 'rack/test'
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
