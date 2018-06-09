0772729f17c30268f988ab672acce3755f6b715a
michael <mdworken@gmail.com>
2018-06-09 11:31:06 -0400

    add test for bikesharefeedprocessor

---
+++ test/models/bikeshare_feed_processor_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+class BikeshareFeedProcessorTest < ActiveSupport::TestCase
+
+  describe 'updating' do
+  
+    before do 
+      
+    end
+
+    it 'should dummy'
+      assert true
+    end 
+
+  end
+
+end
