bdc0bad25308d41f5b4fc60924cd6dc427fb09bf
michael <mdworken@gmail.com>
2018-07-10 16:47:41 -0400

    add dummy landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'location_query#slack_query'
+  get '/bikeshare', to: 'location_query#slack_query'
 
 
   resources :stations, only: [:index]
-  get '/stations/invalid', to: 'stations#invalid'
-  get '/bikeshare_feed_processors/refresh', to: 'bikeshare_feed_processors#manually_refresh'  
-  get '/home', to: 'home#index'
+  get '/stations/invalid', to: 'stations#invalid' #old "test" routes - remove eventually
+  get '/bikeshare_feed_processors/refresh', to: 'bikeshare_feed_processors#manually_refresh' #old "test" routes - remove eventually
+  root 'home#index'
 end
